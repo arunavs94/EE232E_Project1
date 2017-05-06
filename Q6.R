2c5d640a38b4b69b8401f876cada315196d7dd93
bimmeric <ericgoldfien>
2017-05-06 11:33:19 -0700

    found core nodes, now need to analyze their networks

--- Q6.R
+++ Q6.R
@@ -16,11 +16,28 @@ graph_data <- read.table("facebook_combined.txt", sep = "", header = FALSE)
 g1 <- graph.data.frame(graph_data,directed = FALSE) 
 
 #Since the core nodes are likely to belong to the most communities with size greater than 10
-#we want to find all of the core nodes and keep track of all their networks
+#we want to find all the core nodes
+core_nodes <- numeric()
 
-correct_nodes <- numeric()
-deg_corr_nodes <- numeric()
 
 # See which nodes have more than 201 nodes in their network (200 plus core node)
-core_nodes<-which(neighborhood.size(graph, 1 , nodes=V(g1) > 200)
+for (i in 1:vcount(g1)) {
+  net_tmp <- neighborhood(g1, order = 1, nodes = i)
+  pers_net_tmp <- induced.subgraph(g1, vids = unlist(net_tmp), impl = "auto")
+  
+  if (vcount(pers_net_tmp) > 201){
+    core_nodes <- c(core_nodes,i)
+  }
+  
+}
+cat("Number of core nodes =", length(core_nodes))
 
+
+#analyze the networks for each of the core nodes
+for(j in 1:length(core_nodes)){
+  node = core_nodes[c(j)]
+  net_tmp <- neighborhood(g1, order = 1, nodes = node)
+  pers_net_tmp <- induced.subgraph(g1, vids = unlist(net_tmp), impl = "auto")  
+  
+  
+}
