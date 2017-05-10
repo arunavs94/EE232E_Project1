abc42d9e7c0b7be954c145309b2892a8e5e3d79c
arunavs94 <asingh94@ucla.edu>
2017-05-10 00:39:17 -0700

    Includes all plots,labelled, just need to pick which we need

--- Q3_4.R
+++ Q3_4.R
@@ -9,7 +9,7 @@ library("igraph")
 
 # Read in the data from .txt file
 graph_data <- read.table("facebook_combined.txt", sep = "", header = FALSE) # read text file
-g1 <- graph.data.frame(graph_data,directed = TRUE) # covert table to directed graph
+g1 <- graph.data.frame(graph_data,directed = FALSE) # covert table to undirected graph
 
 correct_nodes <- numeric()
 deg_corr_nodes <- numeric()
@@ -19,7 +19,7 @@ for (i in 1:vcount(g1)) {
   net_tmp <- neighborhood(g1, order = 1, nodes = i)
   pers_net_tmp <- induced_subgraph(g1, vids = unlist(net_tmp), impl = "auto")
   
-  if (vcount(pers_net_tmp) > 201){
+  if (vcount(pers_net_tmp) > 200){
     correct_nodes <- c(correct_nodes,i)
     deg_corr_nodes <- c(deg_corr_nodes,degree(g1, v = i))
     
@@ -33,30 +33,36 @@ core_deg_avg <- mean(deg_corr_nodes)
 # Chose a core node (picked 419 aribitrarily)
 core_419 <- neighborhood(g1, order = 1, nodes = 419)
 pers_net_419 <- induced_subgraph(g1, vids = unlist(core_419), impl = "auto")
-# 
-# # Fast Greedy
-# # Generate community 
-# fg_comm_419 <- fastgreedy.community(as.undirected(pers_net_419))
-# 
-# # plot the community (which to use?)
-# plot(fg_comm_419, pers_net_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
-# plot(pers_net_419 , vertex.size=4 , vertex.label=NA , vertex.color=fg_comm_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
-# 
-# # Edge-Betweenness method
-# # Generate community
-# eb_comm_419 <- edge.betweenness.community(pers_net_419)
-# 
-# # plot the community (which to use?)
-# plot(eb_comm_419, pers_net_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
-# plot(pers_net_419 , vertex.size=4 , vertex.label=NA , vertex.color=eb_comm_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
-# 
-# # Infomap method
-# # Generate community
-# im_comm_419 <- infomap.community(pers_net_419)
-# 
-# # plot the community (which to use?)
-# plot(im_comm_419, pers_net_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
-# plot(pers_net_419 , vertex.size=4 , vertex.label=NA , vertex.color=im_comm_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
+
+# Fast Greedy
+# Generate community
+fg_comm_419 <- fastgreedy.community(as.undirected(pers_net_419))
+
+# plot the community (which to use?)
+plot(fg_comm_419, pers_net_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
+title("Fast Greedy with core 1")
+plot(pers_net_419 , vertex.size=4 , vertex.label=NA , vertex.color=fg_comm_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
+title("Fast Greedy with core 2")
+
+# Edge-Betweenness method
+# Generate community
+eb_comm_419 <- edge.betweenness.community(pers_net_419)
+
+# plot the community (which to use?)
+plot(eb_comm_419, pers_net_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
+title("Edge-Betweenness with core 1")
+plot(pers_net_419 , vertex.size=4 , vertex.label=NA , vertex.color=eb_comm_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
+title("Edge-Betweenness with core 2")
+
+# Infomap method
+# Generate community
+im_comm_419 <- infomap.community(pers_net_419)
+
+# plot the community (which to use?)
+plot(im_comm_419, pers_net_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
+title("Infomap with core 1")
+plot(pers_net_419 , vertex.size=4 , vertex.label=NA , vertex.color=im_comm_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
+title("Infomap with core 2")
 
 ####################  QUESTION 4 ####################
 
@@ -71,7 +77,9 @@ fg_comm_uncore_419 <- fastgreedy.community(as.undirected(pers_net_uncore_419))
 
 # plot the community (which to use?)
 plot(fg_comm_uncore_419, pers_net_uncore_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
+title("Fast Greedy w/o core 1")
 plot(pers_net_uncore_419 , vertex.size=4 , vertex.label=NA , vertex.color=fg_comm_uncore_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
+title("Fast Greedy w/o core 2")
 
 # Edge-Betweenness method
 # Generate community
@@ -79,7 +87,9 @@ eb_comm_uncore_419 <- edge.betweenness.community(pers_net_uncore_419)
 
 # plot the community (which to use?)
 plot(eb_comm_uncore_419, pers_net_uncore_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
+title("Edge-Betweenness w/o core 1")
 plot(pers_net_uncore_419 , vertex.size=4 , vertex.label=NA , vertex.color=eb_comm_uncore_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
+title("Edge-Betweenness w/o core 2")
 
 # Infomap method
 # Generate community
@@ -87,4 +97,6 @@ im_comm_uncore_419 <- infomap.community(pers_net_uncore_419)
 
 # plot the community (which to use?)
 plot(im_comm_uncore_419, pers_net_uncore_419, vertex.size=4 , asp = 9/16,vertex.label=NA , edge.color = "grey", layout=layout.fruchterman.reingold)
+title("Infomap w/o core 1")
 plot(pers_net_uncore_419 , vertex.size=4 , vertex.label=NA , vertex.color=im_comm_uncore_419$membership, asp=9/16, layout=layout.fruchterman.reingold)
+title("Infomap w/o core 2")
