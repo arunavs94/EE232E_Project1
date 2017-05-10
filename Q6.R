dddee7063c8faa2a691e61705273e6786ec7b96b
bimmeric <ericgoldfien>
2017-05-10 14:25:37 -0700

    Most up to date Q6

--- Q6.R
+++ Q6.R
@@ -31,13 +31,25 @@ for (i in 1:vcount(g1)) {
   
 }
 cat("Number of core nodes =", length(core_nodes))
-
+cat("\n\n")
 
 #analyze the networks for each of the core nodes
-for(j in 1:length(core_nodes)){
-  node = core_nodes[c(j)]
+#for(i in 1:length(core_nodes)){
+
+
+#testing algorithm on just 1 network before running on all 40
+for(i in 1:1){
+  node = core_nodes[c(i)]
+  
   net_tmp <- neighborhood(g1, order = 1, nodes = node)
   pers_net_tmp <- induced.subgraph(g1, vids = unlist(net_tmp), impl = "auto")  
   
+  pers_net_vertice_count <- vcount(pers_net_tmp)
   
+  cat('Analyzing communities of Core Node # ', i)  
+  cat("\n")
+  cat('Nodes in network: ', pers_net_vertice_count)
+  cat("\n")
+  
+  comm = fastgreedy.community(pers_net_tmp)
 }
