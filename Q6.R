553ff0bbe8a260767ef340d0060c06939f62eba1
arunavs94 <asingh94@ucla.edu>
2017-05-10 17:21:12 -0700

    Finished

--- Q6.R
+++ Q6.R
@@ -25,7 +25,7 @@ for (i in 1:vcount(g1)) {
   net_tmp <- neighborhood(g1, order = 1, nodes = i)
   pers_net_tmp <- induced.subgraph(g1, vids = unlist(net_tmp), impl = "auto")
   
-  if (vcount(pers_net_tmp) > 201){
+  if (vcount(pers_net_tmp) > 200){
     core_nodes <- c(core_nodes,i)
   }
   
@@ -38,7 +38,12 @@ cat("\n\n")
 
 
 #testing algorithm on just 1 network before running on all 40
-for(i in 1:1){
+
+# Feature dictionary
+features <- vector(mode = "list", length = 40)
+
+for(i in 1:40){
+  
   node = core_nodes[c(i)]
   
   net_tmp <- neighborhood(g1, order = 1, nodes = node)
@@ -51,5 +56,35 @@ for(i in 1:1){
   cat('Nodes in network: ', pers_net_vertice_count)
   cat("\n")
   
+  comm_10plus = 0
   comm = fastgreedy.community(pers_net_tmp)
+  
+  features[[i]] <- vector(mode = "list", length = length(which(sizes(comm) > 10)))
+  count <- 1
+  for(j in 1:length(sizes(comm))){
+    if(sizes(comm)[j] > 10){
+      comm_nodes = which(comm$membership==j)
+      temp_comm = induced.subgraph(pers_net_tmp, comm_nodes)
+      comms_comm = fastgreedy.community(temp_comm)
+      cat('Analyzing Community # ', count)
+      cat("\n")
+      
+      cat('Community size =' ,vcount(temp_comm))
+      cat("\n")
+      cat('Modularity =' , modularity(comms_comm))
+      cat("\n")
+      cat('Density =' , graph.density(temp_comm, loops = TRUE))
+      cat("\n")
+      cat('Clustering Coeffifient =', transitivity(temp_comm))
+      cat("\n\n")
+      
+      # Load values into feature dictionary
+      features[[i]][[count]] <- c(modularity(comms_comm),transitivity(temp_comm), graph.density(temp_comm, loops = TRUE),vcount(temp_comm)) # ModIndex, ClusertingCoeff, Density, CommSize
+      count <- count + 1
+    } 
+  }
+  
+  
+  
+  
 }
